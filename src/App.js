d70bce7fbad38bef04cd45ae912912c146a77c2b
Farhad <farhad.abbasi9@gmail.com>
2021-07-11 20:27:45 +0430

    added first props for dynamic attribute called text for title of todo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learning React"/>
+      <Todo text="Mastering React"/>
+      <Todo text="Exploring the full React course"/>
     </div>
   );
 }
