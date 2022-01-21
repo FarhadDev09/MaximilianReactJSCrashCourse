67e8cdf303ddcaa52f8b4a08c6952f717d7556a7
Farhad Abbasi <farhad.abbasi9@gmail.com>
2022-01-22 00:17:50 +0330

    using useState for opening and closing delete modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
-  return (
-    <div className="card">
-      <h2>{props.text}</h2>
-      <div className="actions">
-        <button className="btn">Delete</button>
-      </div>
-    </div>
-  );
+  const [modalIsOpen, setModalOpen] = useState(false);
+
+    function deleteHandler() {
+        setModalOpen(true);
+    }
+
+    function closeModalHandler() {
+      setModalOpen(false);
+    }
+
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+            <div className="actions">
+                <button className="btn" onClick={deleteHandler}>
+                    Delete
+                </button>
+            </div>
+            {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+            {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
+        </div>
+    );
 }
 
 export default Todo;
